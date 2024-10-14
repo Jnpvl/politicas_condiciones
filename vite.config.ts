457df83e347bf8104cf1d59791273acacbebab14
Jnpvl <151414138+Jnpvl@users.noreply.github.com>
2024-10-14 07:31:59 -0700

    da

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: '/politicas_condiciones/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
